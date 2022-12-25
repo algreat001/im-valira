223ce051d23eceb42ee4fb487499fab5d788016e
alexei.shkolnik <alexei.shkolnik@trember.com>
2022-12-25 19:45:53 +0100

    back - add telegram bot

--- src/send/send.service.ts
+++ src/send/send.service.ts
@@ -1,30 +1,57 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { TelegramService } from '../telegram/telegram.service';
+import { NodeHtmlMarkdown } from 'node-html-markdown';
 
 @Injectable()
 export class SendService {
-  constructor(private readonly mailerService: MailerService) {
-
-  }
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   async sendOrder(email: string, orderHtml: string): Promise<boolean> {
-    return this.sendMail(email, "Заказ в интернет магазине \"Valira-decor\"", orderHtml);
+    const telegramMessage =
+      '*Новый заказ в интернет магазине "Valira decor"*\n' +
+      NodeHtmlMarkdown.translate(orderHtml, {})
+        .replace('\n|', '\n')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .replace('| ----------------- | --------------------- | ------ | ---------- |','');
+
+    return (
+      await Promise.all([
+        this.sendMail(
+          email,
+          'Заказ в интернет магазине "Valira-decor"',
+          orderHtml,
+        ),
+        this.telegramService.sendMessage(telegramMessage),
+      ])
+    ).reduce((accum, current) => accum && current, true);
   }
-  
-  private async sendMail(email: string, subj: string, body: string) {
-    const addr = "office@energy-soft.ru";
+
+  private async sendMail(
+    email: string,
+    subj: string,
+    body: string,
+  ): Promise<boolean> {
+    const addr = 'office@energy-soft.ru';
     try {
       const res1 = await this.mailerService.sendMail({
-        from: "office@energy-soft.ru",
+        from: 'office@energy-soft.ru',
         to: addr,
         subject: subj,
-        html: body
+        html: body,
       });
       const res2 = await this.mailerService.sendMail({
-        from: "office@energy-soft.ru",
+        from: 'office@energy-soft.ru',
         to: email,
         subject: subj,
-        html: body
+        html: body,
       });
       return res1 && res2;
     } catch (error) {
@@ -32,5 +59,4 @@ export class SendService {
       return false;
     }
   }
-
 }
---
+++ src/telegram/telegram.config.ts
@@ -0,0 +1,4 @@
+export class TelegramOptions {
+  chatId = process.env.TELEGRAM_CHAT_ID;
+  token = process.env.TELEGRAM_CHAT_BOT_TOKEN;
+}
